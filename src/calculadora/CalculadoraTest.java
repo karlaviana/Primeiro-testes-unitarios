642ea4f3886d7f8a71efbde471ed1f4a9fce4cb3
karlaviana <karlavianacosta@gmail.com>
2016-04-20 15:48:27 -0300

    Update CalculadoraTest.java
    Novas funcionalidades implementadas

--- src/calculadora/CalculadoraTest.java
+++ src/calculadora/CalculadoraTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 public class CalculadoraTest {
 
+
 	@Test
 	public void testSomar(){
 	  int result = Calculadora.Soma(2,3);
@@ -13,13 +14,25 @@ public class CalculadoraTest {
 	  assertEquals(result, esperado);		
 		}
 	@Test
+	public void testSomarNegativos(){
+	  int result = Calculadora.Soma(-2,3);
+	  int esperado= 1;		  
+	  assertEquals(result, esperado);		
+		}
+	
+	@Test
 	public  void testSubtração(){
 		int result = Calculadora.Subtração(0, 0);
 		int esperado = 0;
 		assertEquals(result, esperado);	
 	}
+	@Test
+	public  void testSubtrirNegativos(){
+		int result = Calculadora.Subtração(-2, 0);
+		int esperado = -2;
+		assertEquals(result, esperado);	
+	}
 	
-}
 	@Test
 	 public void testMultiplicação(){
 		int result = Calculadora.Multiplicação(0, 0);
@@ -27,11 +40,26 @@ public class CalculadoraTest {
 		assertEquals(result, esperado);	
 	}
 	@Test
+	 public void testMultiplicarNegativos(){
+		int result = Calculadora.Multiplicação(45, -2);
+		int esperado = -90;
+		assertEquals(result, esperado);
+	
+}
+ 
+	@Test
 	public void TestDivsão(){
 		int result = Calculadora.Divisão(3,2);
 		int esperado = 1;
 	    assertEquals(result, esperado);
 	}
-	}
+	
+	@Test
+	public void testDivsãoCaracteresEspeciais(){
+		int result = Calculadora.Divisão(3,2);
+		String variavel = "!";
+		int esperado = 1;
+	    assertEquals(result, esperado);
+	}  
 
 
