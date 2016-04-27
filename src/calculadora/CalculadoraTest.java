eb74bcc5cfbcdb91e392840e65d0d04cfc1f0d2e
karlaviana <karlavianacosta@gmail.com>
2016-04-27 10:23:30 -0300

    Update CalculadoraTest.java
    novo codigo da calculadoraTeste

--- src/calculadora/CalculadoraTest.java
+++ src/calculadora/CalculadoraTest.java
@@ -1,65 +1,36 @@
-package calculadora;
+package com.calculadoraTest;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.calculadora.Calculadora;
+
 public class CalculadoraTest {
 
-
 	@Test
-	public void testSomar(){
-	  int result = Calculadora.Soma(2,3);
-	  int esperado= 5;		  
-	  assertEquals(result, esperado);		
-		}
-	@Test
-	public void testSomarNegativos(){
-	  int result = Calculadora.Soma(-2,3);
-	  int esperado= 1;		  
-	  assertEquals(result, esperado);		
-		}
-	
-	@Test
-	public  void testSubtração(){
-		int result = Calculadora.Subtração(0, 0);
-		int esperado = 0;
-		assertEquals(result, esperado);	
+	public void TestarSoma() {
+		Calculadora calc = new Calculadora();
+		assertEquals(calc.Somar(1, 1),2);
+		assertEquals(calc.Somar(1, 0),1);
+		assertEquals(calc.Somar(1, -1),0);
+		
 	}
-	@Test
-	public  void testSubtrirNegativos(){
-		int result = Calculadora.Subtração(-2, 0);
-		int esperado = -2;
-		assertEquals(result, esperado);	
-	}
-	
-	@Test
-	 public void testMultiplicação(){
-		int result = Calculadora.Multiplicação(0, 0);
-		int esperado = 0;
-		assertEquals(result, esperado);	
-	}
-	@Test
-	 public void testMultiplicarNegativos(){
-		int result = Calculadora.Multiplicação(45, -2);
-		int esperado = -90;
-		assertEquals(result, esperado);
-	
+   @Test
+   public void TestSubtrair(){
+	   Calculadora calc = new Calculadora();
+	   assertTrue(calc.subtrair(1,1)== 0);
+	   assertTrue(calc.subtrair(2,2)== 0);
+	   assertTrue(calc.subtrair(0, 0)== 0);
+   }
+   public void TestMultiplicar(){
+	   Calculadora calc = new Calculadora();
+	   assertEquals(calc.multiplicar(1, 1),2);
+	   assertEquals(calc.multiplicar(2, 1),2);
+   }
+   public void TestDividir(){
+	   Calculadora calc = new Calculadora();
+	   assertEquals(calc.dividir (1,1),0);
+	  
 }
- 
-	@Test
-	public void TestDivsão(){
-		int result = Calculadora.Divisão(3,2);
-		int esperado = 1;
-	    assertEquals(result, esperado);
-	}
-	
-	@Test
-	public void testDivsãoCaracteresEspeciais(){
-		int result = Calculadora.Divisão(3,2);
-		String variavel = "!";
-		int esperado = 1;
-	    assertEquals(result, esperado);
-	}  
-
-
+}
