39de1bc5fca81209f7c417c7d01107cb9a1c683c
karlaviana <karlavianacosta@gmail.com>
2016-04-28 10:45:13 -0300

    Update CalculadoraTest.java

--- src/calculadora/CalculadoraTest.java
+++ src/calculadora/CalculadoraTest.java
@@ -22,15 +22,19 @@ public class CalculadoraTest {
 	   assertTrue(calc.subtrair(1,1)== 0);
 	   assertTrue(calc.subtrair(2,2)== 0);
 	   assertTrue(calc.subtrair(0, 0)== 0);
-   }
-   public void TestMultiplicar(){
-	   Calculadora calc = new Calculadora();
-	   assertEquals(calc.multiplicar(1, 1),2);
-	   assertEquals(calc.multiplicar(2, 1),2);
-   }
-   public void TestDividir(){
-	   Calculadora calc = new Calculadora();
-	   assertEquals(calc.dividir (1,1),0);
-	  
+    
 }
-}
+  @Test 
+  public void TestMultiplicar(){
+	  Calculadora calc = new Calculadora();
+	  assertEquals(calc.multiplicar(1,1),1);
+	  assertEquals(calc.multiplicar(2, 1),2);
+	  assertEquals(calc.multiplicar(2, 3),6);
+  }
+  @Test
+  public void TestDividir(){
+	  Calculadora calc = new Calculadora();
+	  assertEquals(calc.dividir(2,1),2);
+	  assertEquals(calc.dividir(4,1),4);
+  }
+  
